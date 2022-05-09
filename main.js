3504e33584df23a665de61711ad0e7d38d94f063
wHarrisonn <99509845+wHarrisonn@users.noreply.github.com>
2022-05-08 22:36:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activeMenuAtCurrentSection(home)
+  activeMenuAtCurrentSection(services)
+  activeMenuAtCurrentSection(about)
+  activeMenuAtCurrentSection(contact)
+}
+
+function activeMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndsPassedTargetLine = sectionEndsAt <= targetLine
+
+  //Limites da seção
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndsPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navegation.classList.add('scroll')
+  } else {
+    navegation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
